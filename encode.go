d67b2c303f0731dbab6491fde3b752d3db48680d
Oleg Kovalov <oleg@hey.com>
2023-08-16 21:16:59 +0200

    fix

--- encode.go
+++ encode.go
@@ -22,29 +22,31 @@ func NewEncoder(w io.Writer) *Encoder {
 }
 
 // Encode writes the BSON encoding of v to the stream.
-func (e *Encoder) Encode(v any) error {
-	e.buf.Reset()
-	if err := e.marshal(v); err != nil {
+func (enc *Encoder) Encode(v any) error {
+	enc.buf.Reset()
+	if err := enc.marshal(v); err != nil {
 		return fmt.Errorf("encode failed: %w", err)
 	}
-	_, err := e.w.Write(e.buf.Bytes())
+	_, err := enc.w.Write(enc.buf.Bytes())
 	return err
 }
 
-func (e *Encoder) marshal(v any) error {
+func (enc *Encoder) marshal(v any) error {
 	switch v := v.(type) {
+	case MarshalerTo:
+		return v.MarshalBSON(enc.buf)
 	case Marshaler:
 		raw, err := v.MarshalBSON()
 		if err != nil {
 			return err
 		}
-		e.buf.Write(raw)
+		enc.buf.Write(raw)
 	case A:
-		e.marshalArray(v)
+		enc.marshalArray(v)
 	case D:
-		e.marshalDoc(v)
+		enc.marshalDoc(v)
 	case M:
-		e.marshalDoc(v.AsD())
+		enc.marshalDoc(v.AsD())
 	default:
 		return fmt.Errorf("type %T is not supported yet", v)
 	}
@@ -62,7 +64,7 @@ func (enc *Encoder) marshalArray(arr A) error {
 	return enc.marshalDoc(doc)
 }
 
-func (e *Encoder) marshalDoc(doc D) error {
+func (enc *Encoder) marshalDoc(doc D) error {
 	// TODO(cristaloleg): prealloc or smarter way.
 	var elist bytes.Buffer
 
@@ -72,38 +74,38 @@ func (e *Encoder) marshalDoc(doc D) error {
 
 		switch v := pair.V.(type) {
 		case string:
-			e.writeKey(&elist, TypeString, key)
+			enc.writeKey(&elist, TypeString, key)
 			b := putUint32(uint32(len(v) + 1))
 			elist.Write(b[:])
 			elist.WriteString(v)
 			elist.WriteByte(0)
 
 		case int32:
-			e.writeKey(&elist, TypeInt32, key)
+			enc.writeKey(&elist, TypeInt32, key)
 			b := putUint32(uint32(v))
 			elist.Write(b[:])
 
 		case int64:
-			e.writeKey(&elist, TypeInt64, key)
+			enc.writeKey(&elist, TypeInt64, key)
 			b := putUint64(uint64(v))
 			elist.Write(b[:])
 
 		case bool:
-			e.writeKey(&elist, TypeBool, key)
+			enc.writeKey(&elist, TypeBool, key)
 			elist.WriteByte(putBool(v))
 		}
 	}
 
 	size := 4 + elist.Len() + 1 // header + len + null.
 	b := putUint32(uint32(size))
-	e.buf.Write(b[:])
+	enc.buf.Write(b[:])
 
-	io.Copy(e.buf, &elist)
-	e.buf.WriteByte(0)
+	io.Copy(enc.buf, &elist)
+	enc.buf.WriteByte(0)
 	return nil
 }
 
-func (e *Encoder) writeKey(buf *bytes.Buffer, t Type, s string) {
+func (enc *Encoder) writeKey(buf *bytes.Buffer, t Type, s string) {
 	buf.WriteByte(byte(t))
 	buf.WriteString(s)
 	buf.WriteByte(0)
