201f549873c2d763daa1f9de3bb82ed7c061e133
Oleg Kovalov <oleg@hey.com>
2023-08-18 18:33:33 +0200

    Add marshaling via reflect

--- encode_test.go
+++ encode_test.go
@@ -94,3 +94,77 @@ func TestEncodeM(t *testing.T) {
 	wantBytes(t, buf.Bytes(), "1b0000001061000a000000126200a0af9b00000000000863000100")
 	buf.Reset()
 }
+
+func TestEncodeReflectMap(t *testing.T) {
+	var buf bytes.Buffer
+	enc := NewEncoder(&buf)
+
+	var err error
+	var m map[string]any
+
+	m = map[string]any{}
+	err = enc.Encode(m)
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "0500000000")
+	buf.Reset()
+
+	m = map[string]any{"abc": int32(123)}
+	err = enc.Encode(m)
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "0e00000010616263007b00000000")
+	buf.Reset()
+
+	m = map[string]any{"hello": "world", "foo": int32(123)}
+	err = enc.Encode(m)
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "1f0000000268656c6c6f0006000000776f726c640010666f6f007b00000000")
+	buf.Reset()
+}
+
+func TestEncodeReflectSlice(t *testing.T) {
+	var buf bytes.Buffer
+	enc := NewEncoder(&buf)
+
+	var err error
+	var s []string
+
+	s = []string{}
+	err = enc.Encode(s)
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "0500000000")
+	buf.Reset()
+
+	s = []string{"abc"}
+	err = enc.Encode(s)
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "10000000023000040000006162630000")
+	buf.Reset()
+
+	s = []string{"hello", "world"}
+	err = enc.Encode(s)
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "1f0000000230000600000068656c6c6f0002310006000000776f726c640000")
+	buf.Reset()
+}
+
+func TestEncodeReflectArray(t *testing.T) {
+	var buf bytes.Buffer
+	enc := NewEncoder(&buf)
+
+	var err error
+
+	err = enc.Encode([0]string{})
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "0500000000")
+	buf.Reset()
+
+	err = enc.Encode([1]string{"abc"})
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "10000000023000040000006162630000")
+	buf.Reset()
+
+	err = enc.Encode([...]string{"hello", "world"})
+	mustOk(t, err)
+	wantBytes(t, buf.Bytes(), "1f0000000230000600000068656c6c6f0002310006000000776f726c640000")
+	buf.Reset()
+}
